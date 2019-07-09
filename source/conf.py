6d34bac751596e39790662a62f07135a008e36fe
Ruixiang Du <ruixiang.du@gmail.com>
2019-07-09 03:22:28 -0400

    updated copyright info

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RDU-Wiki'
-copyright = u'2017, Ruixiang Du'
+copyright = u'2019, Ruixiang Du'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
